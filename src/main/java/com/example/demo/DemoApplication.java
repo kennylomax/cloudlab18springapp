bf83c75bf1b8764d50d287178d11e7a686e0a767
kennylomax <kennylomax@gmail.com>
2021-04-06 17:07:44 +0200

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// mvn spring-boot:run  -Dspring-boot.run.arguments=--server.port=8085
 @RestController
 @SpringBootApplication
 public class DemoApplication {
@@ -15,8 +14,6 @@ public class DemoApplication {
 
     @RequestMapping("/hithere")
     public String visitHome() {
-      //  int a = 2;
-      //  int b = a;
         return "42";
     }
     
